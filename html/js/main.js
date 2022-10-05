99bc6f199a92e69d498fd9c35610433b019a969b
Niloy Islam <110332742+niloy-islam@users.noreply.github.com>
2022-10-05 21:45:05 +0600

    Add files via upload

---
+++ html/js/main.js
@@ -0,0 +1,9 @@
+// bx slider
+$(document).ready(function(){
+  $('.slider').bxSlider();
+ $('#main-menu').meanmenu({
+     meanMenuContainer: '#mobile-menu',
+      meanScreenWidth:991,
+  }
+ );
+});
